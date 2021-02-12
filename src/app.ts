dca371c855ba7aa25c7452e22bea23e46925c289
Joshua Abad <yeyoan@gmail.com>
2021-02-13 00:00:39 +0800

    Use third-party logging middleware

--- src/app.ts
+++ src/app.ts
@@ -15,9 +15,10 @@ import history from 'connect-history-api-fallback'
 import compression from 'compression'
 import verifyRouter from './controllers/verify'
 import validateRouter from './controllers/validate'
+import morgan from 'morgan'
 import helmet from 'helmet'
 
-logger.info('connecting to', config.MONGODB_URI as string)	
+logger.info('Connecting to MongoDB...')	
 
 mongoosee.connect(config.MONGODB_URI as string, {
   useNewUrlParser: true,
@@ -25,9 +26,9 @@ mongoosee.connect(config.MONGODB_URI as string, {
   useFindAndModify: false,
   useCreateIndex: true
 }).then(() => {
-  logger.info('connected to MongoDB')	
+  logger.info('Connected to MongoDB')	
 }).catch(error => {
-  logger.error('error connecting to MongoDB:', (error as Error).message)	
+  logger.error('Error connecting to MongoDB: %s', (error as Error).message)	
 })
 
 const app = express()
@@ -36,7 +37,14 @@ app.use(helmet())
 app.use(compression())
 app.use(cors())
 app.use(express.json())
-app.use(middleware.requestLogger)
+
+app.use(morgan('tiny', {
+  stream: {
+    write (str) {
+      logger.info(str.replace(/\n$/, ''))
+    }
+  }
+}))
 
 app.use('/users', usersRouter)
 app.use('/courses', coursesRouter)
--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ import logger from './utils/logger'
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)	
+  logger.info(`App served at http://localhost:${config.PORT}`)	
 })
